bbac75f0cb8b4394d0b7eb6df016772fc0a0e72d
Grzegorz Judas <grzegorz.judas@gmail.com>
2019-02-20 21:53:49 +0100

    Add compression middleware

--- src/server/middleware/index.ts
+++ src/server/middleware/index.ts
@@ -1,8 +1,10 @@
 /* Application files */
+import CompressionMiddleware from './CompressionMiddleware';
 import CrossOriginHeadersMiddleware from './CrossOriginHeadersMiddleware';
 import RequestDataJsonParserMiddleware from './RequestDataJsonParserMiddleware';
 
 export default [
+    CompressionMiddleware(),
     CrossOriginHeadersMiddleware(),
     RequestDataJsonParserMiddleware()
 ];
