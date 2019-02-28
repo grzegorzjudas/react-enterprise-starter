1cbed1b7a02af291ca761eda95e86bf9f80876c2
Grzegorz Judas <grzegorz.judas@gmail.com>
2019-02-28 19:53:04 +0100

    Configured jest and add initial unit tests for App component

---
+++ src/client/components/App/App.spec.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow, ShallowWrapper } from 'enzyme';
+import Component from './App';
+
+function renderShallow (props = {}) {
+    return shallow(<Component {...props} />);
+}
+
+describe('App', () => {
+    let component: ShallowWrapper;
+    let props = {};
+
+    beforeEach(() => component = renderShallow(props));
+
+    it('renders correctly', () => {
+        expect(component.find('div').length).toBe(1);
+    });
+
+    it('renders correct content', () => {
+        expect(component.find('div').text()).toBe('Hello world!');
+    });
+});
