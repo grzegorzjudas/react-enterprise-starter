e8387a3fb4559f5291676c63771826140af3a765
Grzegorz Judas <grzegorz.judas@gmail.com>
2019-03-31 11:04:36 +0200

    Disable sandbox on production environment

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,9 +1,23 @@
 const def = require('./jest.config');
 
+function switchEnvs (dev, prod) {
+    if (process.env.NODE_ENV === 'development') return dev;
+
+    return prod;
+}
+
 const config = Object.assign(def, {
     launch: {
         dumpio: true,
-        headless: ![ '', 'development' ].includes(process.env.NODE_ENV)
+        headless: switchEnvs(false, true),
+        args: switchEnvs([], [
+            '--no-sandbox',
+            '--disable-setuid-sandbox'
+        ]),
+        defaultViewport: {
+            width: 1920,
+            height: 1080
+        }
     },
     preset: 'jest-puppeteer',
     browserContext: 'default',
